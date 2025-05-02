d98d1886ac65f51a7f5bb6c335e8dfd7dce0a590
Klayton G <klayton.goering@gmail.com>
2025-05-02 10:57:51 -0500

    coding examples for 5-length variables, (& reverse coding for counter-indicitive items within those variables)

--- R exercise 1/LIkertScaling_SDQ.R
+++ R exercise 1/LIkertScaling_SDQ.R
@@ -1,1 +1,13 @@
-print("Hey")
+print("Strengths&Difficulties")
+
+items_hyperactivity <- c("restles","fidgety","distrac","reflect","attends")
+
+items_emotion <- c("somatic","worries","unhappy","clingy","afraid")
+rowMeans(SDQ[items_emotion], na.rm = T)*5
+SDQ$S_emotion <- rowMeans(SDQ[items_emotion], na.rm=TRUE)*5
+
+R_peerprobs2 < reverse.code(keys = c(1,-1,-1,1,1), SDQ[items_peerprobs2])
+SDQ$S_peerprobs2 <- rowMeans(R_peerprobs2, na.rm = T)*5
+
+
+
